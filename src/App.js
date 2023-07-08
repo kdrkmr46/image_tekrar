072081296c0c758071fb9c9c0ef5605808c41624
kdrkmr46 <kdrkmr46@gmail.com>
2023-07-08 14:36:49 +0300

    images import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Picture from "./components/Picture";
 import data from "./helpers/data";
+import "./style/style.css";
 
 function App() {
   return (
     <div className="App">
+      <h1>albüms</h1>
       {data.map((item) => {
         return <Picture img={item.src.large} photoname={item.photographer} />;
       })}
--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -1,8 +1,8 @@
 const Picture = ({ img, photoname }) => {
   return (
     <div>
-      <img src={img} alt={img} />
-      <span> {photoname} </span>
+      <img className="imageContainer" src={img} alt={img} />
+      <span className="css"> {photoname} </span>
     </div>
   );
 };
